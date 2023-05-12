64c5569b97f1a3d9b16290ffd5ef73ee969c1677
Douglas Vieira da Silva <ieiradouglas@gmail.com>
2023-05-11 21:43:13 -0300

    Alterando links

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -36,9 +36,8 @@ export default function Galeria(){
         <Card onClick={()=>setIsVisible(true)}/>
         <Card onClick={()=>setIsVisible(true)}/>
         <Card onClick={()=>setIsVisible(true)}/>
-        <Card onClick={()=>setIsVisible(true)}/>
       </section>
-      <a className=" fixed bottom-2 right-2 w-[55px] h-[55px] bg-[#FAA307] rounded-xl flex items-center justify-center z-50 duration-150 hover:scale-110 _blank" href="https://api.whatsapp.com/send/?phone=%2B5522999401838&text&type=phone_number&app_absent=0"><img className="w-[35px] h-[35px]" src="/wppicon.svg" alt="Whatsapp ícone" /></a>
+      <a className=" fixed bottom-2 right-2 w-[55px] h-[55px] bg-[#FAA307] rounded-xl flex items-center justify-center z-50 duration-150 hover:scale-110 _blank" href="/"><img className="w-[35px] h-[35px]" src="/wppicon.svg" alt="Whatsapp ícone" /></a>
     </>
   )
 }
